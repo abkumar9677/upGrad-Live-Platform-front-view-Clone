748d1e024b24111aa29cde5cd86cd162b20dc886
abkumar9677 <abhi967792@gmail.com>
2021-01-08 10:21:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vqnxqe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vqnxqe)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <Heading value="upGrad">
+
+
+
+
+    </div>
+  );
+}
