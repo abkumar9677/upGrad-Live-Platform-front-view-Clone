dd0a7195314ea20b0084edc099bbf032c90d6f73
abkumar9677 <abhi967792@gmail.com>
2021-01-08 16:01:29 +0530

    Navbar creation completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
+import { render } from "react-dom";
 import Heading from "./components/Header.js";
-export default function App() {
-  return (
-    <div>
-      <Heading value="upGrad" />
-    </div>
-  );
+
+// export default function App() {
+class App extends Component {
+  constructor() {
+    super();
+    var today = new Date(),
+      time = today.getHours() + ":" + today.getMinutes();
+    this.state = {
+      currentTime: time
+    };
+  }
+  render() {
+    return (
+      <fragment>
+        <Heading value="upGrad" />
+        <div className="time-slot">
+          <i className="fa fa-clock-o clock" aria-hidden="true" />
+          <span className="time"> {this.state.currentTime}</span>
+          <span className="help"><span>Help <i class="fa fa-headphones head" aria-hidden="true"></i> </span>          
+          <button className="leave">Leave</button>
+          </span>
+        </div>
+      </fragment>
+    );
+  }
 }
+
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Header.css";
 
 const Heading = function(props) {
-  return <div className="header">{props.value}</div>;
+  return [
+    <div>
+      <div className="header">{props.value}</div>
+    </div>
+  ];
 };
 export default Heading;
