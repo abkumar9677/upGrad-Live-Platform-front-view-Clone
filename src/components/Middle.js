ea29ec229977b8dbfc856ed777c00f1e576f27e4
abkumar9677 <abhi967792@gmail.com>
2021-01-09 00:03:19 +0530

    Work completed and Platform view is somehow similarly made.

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -7,8 +7,33 @@ class Middle extends Component {
       <fragment>
         <br />
         <div className="mid-screen">
-          <div className="agenda" />
-          <div className="agenda2" style={{ float: "right" }} />
+          <div className="agenda">
+            <div className="head">
+              <span>Showcase : Live Platform Clone </span>
+              <br />
+              <br />
+              <span>by Abhishek Kumar</span>
+            </div>
+            <hr />
+            <ul>
+              <h4>Agenda</h4>
+              <li>For Practice Reasons.</li>
+              <li>For Revising some concept.</li>
+            </ul>
+          </div>
+          <div className="agenda2" style={{ float: "right" }}>
+            <div className="head">
+              <p>
+                "This work is only done for practice
+                <br />
+                <br /> purpose to furnish my ReactJS basic concepts."
+                <br />
+                <br />
+                "This whole view is of the upGrad live platform.
+                <br /> And I daily take lectures on this platform."
+              </p>
+            </div>
+          </div>
         </div>
       </fragment>
     );
